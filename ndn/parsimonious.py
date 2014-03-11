8430d50850d8742b77a91aa704bf0717d38faa80
Andrew Lorente <andrew.lorente@gmail.com>
2014-03-10 19:58:50 -0700

    Parse decimal numbers [parsimonious]
    Numbers are now a series of digits, possibly with a . in the middle of
    them. This parser doesn't accept inputs like "15." or ".15", which are
    arguably valid input. Personally I figure it won't kill the user to type
    a 0 once in a while.
    
    generic_visit might need some explanation. parsimonious's NodeVisitors
    call generic_visit when the group they're matching doesn't have a
    particular name. Rules like the `number` rule, which consists of several
    parts, are implicitly transformed into a named rule and several
    anonymous rules that're visited by generic_visit. We don't care about
    the lower-level matches, just the outermost number, so generic_visit
    doesn't need any functionality. However, the default implentation in
    NodeVisitor raises a NotImplementedError, so we have to override it with
    an empty method.

--- ndn/parsimonious.py
+++ ndn/parsimonious.py
@@ -4,12 +4,15 @@ from parsimonious.grammar import Grammar
 from parsimonious.nodes import NodeVisitor
 
 grammar = Grammar("""
-    number = ~"[0-9]+"
+    number = ~"[0-9]+" ("." ~"[0-9]+")?
 """)
 
 class Calculator(NodeVisitor):
+    def generic_visit(self, node, visited_children):
+        pass
+
     def visit_number(self, node, visited_children):
-        return int(node.text)
+        return float(node.text)
 
 def calculate(text):
     return Calculator().visit(grammar.parse(text))
--- test/test_parsimonious.py
+++ test/test_parsimonious.py
@@ -1,8 +1,16 @@
 from unittest import TestCase
-from nose.tools import eq_
+from parsimonious.exceptions import IncompleteParseError
+from nose.tools import eq_, raises
 
 from ndn.parsimonious import calculate
 
 class TestParse(TestCase):
     def test_parse_a_single_number(self):
         eq_(1, calculate('1'))
+
+    def test_parse_a_decimal_number(self):
+        eq_(3.141, calculate('3.141'))
+
+    @raises(IncompleteParseError)
+    def test_numbers_can_have_at_most_one_decimal_point(self):
+        calculate('2.7.2')
