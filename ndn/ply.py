6c51771adbc6d2bb5610715f294951fbb7d30386
Andrew Lorente <hello@andrewlorente.com>
2014-03-12 20:51:34 -0700

    Add various operators [ply]
    Now you can see what I meant about a nice clean design from returning
    functions in the tokenizer. Each operator token returns the associated
    function, and the parser just calls it. In a more classic yacc
    implementation, the operator tokens would just return their string
    representations, and you'd have a big if-elsif-elsif-elsif block where
    you look up the behavior associated with the strings.
    
    You may notice that the number rule changed here. Previously a unary '-'
    was considered part of the numeric token, whereas now it's a separate
    operator. It turns out that with two different ways to interpret the '-'
    token, you end up with an ambiguous grammar and the behavior is
    unpredictable. Having the unary '-' pass an explicit 0 to the binary
    subtraction function is either an elegant workaround or a kludgy hack,
    depending on your viewpoint.

--- ndn/ply.py
+++ ndn/ply.py
@@ -2,14 +2,19 @@ from __future__ import absolute_import
 
 import ply.lex as lex
 import ply.yacc as yacc
+from random import randint
 
 class SilentLogger(object):
     def warning(*args, **kwargs):
         pass
 
 tokens = (
-    'PLUS',
     'NUMBER',
+    'PLUS',
+    'MINUS',
+    'MULTIPLY',
+    'DIVIDE',
+    'ROLL',
 )
 
 def t_PLUS(token):
@@ -17,16 +22,46 @@ def t_PLUS(token):
     token.value = lambda a, b: a + b
     return token
 
+def t_MINUS(token):
+    r'-'
+    token.value = lambda a, b: a - b
+    return token
+
+def t_MULTIPLY(token):
+    r'\*'
+    token.value = lambda a, b: a * b
+    return token
+
+def t_DIVIDE(token):
+    r'/'
+    token.value = lambda a, b: a / b
+    return token
+
+def t_ROLL(token):
+    r'd'
+    def roll(num, size):
+        return sum(map(lambda x: randint(size), range(0, int(num))))
+    token.value = roll
+    return token
+
 def t_NUMBER(token):
-    r'(-?)[0-9]+(\.[0-9]+)?'
+    r'[0-9]+(\.[0-9]+)?'
     token.value = float(token.value)
     return token
 
 lexer = lex.lex(errorlog=SilentLogger())
 
 def p_expression_operator(p):
-    'expression : expression PLUS expression'
+    '''expression : expression PLUS expression
+                  | expression MINUS expression
+                  | expression MULTIPLY expression
+                  | expression DIVIDE expression
+                  | expression ROLL expression'''
     p[0] = p[2](p[1], p[3])
+
+def p_unary_minus(p):
+    'expression : MINUS expression'
+    p[0] = p[1](0, p[2])
 
 def p_number(p):
     'expression : NUMBER'
--- test/test_ply.py
+++ test/test_ply.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from nose.tools import eq_, raises
+from mock import patch
 
 from ndn.ply import calculate
 
@@ -19,8 +20,30 @@ class TestPlyParse(TestCase):
     def test_parse_a_negative_number(self):
         eq_(-1, calculate('-1'))
 
+    def test_perform_operations_on_a_negative_number(self):
+        eq_(-5, calculate('10/-2'))
+
     def test_add_two_numbers(self):
         eq_(7, calculate('5+2'))
 
     def test_add_several_numbers(self):
         eq_(32, calculate('8+14+3+7'))
+
+    def test_multiply_two_numbers(self):
+        eq_(48, calculate('6*8'))
+
+    def test_divide_two_numbers(self):
+        eq_(2.5, calculate('5/2'))
+
+    def test_subtract_two_numbers(self):
+        eq_(8, calculate('13-5'))
+
+    def test_operators_are_right_associative(self):
+        eq_((23 - (10 - 5)), calculate('23-10-5'))
+        eq_((24 / (16 / 2)), calculate('24/16/2'))
+        eq_((8 + (5 * 3)), calculate('8+5*3'))
+
+    @patch('ndn.ply.randint')
+    def test_roll_the_dice(self, mock_randint):
+        mock_randint.return_value = 5
+        eq_(10, calculate('2d6'))
