d7bbb794bd21a24b8c2c0741e2cc1a1375a6905c
Andrew Lorente <hello@andrewlorente.com>
2014-04-09 19:47:20 -0700

    Add support for decimal numbers [parsley]
    Numbers are now a series of digits, possibly followed by a dot and a
    second series of digits. We also had to switch from using int() to
    float() to correctly support these numbers!

--- ndn/parsley.py
+++ ndn/parsley.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from parsley import makeGrammar
 
 grammar = makeGrammar("""
-    number = <digit+>:ds -> int(ds)
+    number = <digit+ ('.' digit+)?>:ds -> float(ds)
 """, {})
 
 def calculate(text):
--- test/test_parsley.py
+++ test/test_parsley.py
@@ -1,8 +1,16 @@
 from unittest import TestCase
-from nose.tools import eq_
+from nose.tools import eq_, raises
+from parsley import ParseError
 
 from ndn.parsley import calculate
 
 class TestParsleyParse(TestCase):
     def test_parse_a_single_number(self):
         eq_(1, calculate('1'))
+
+    def test_parse_a_decimal_number(self):
+        eq_(3.141, calculate('3.141'))
+
+    @raises(ParseError)
+    def test_numbers_can_have_at_most_one_decimal_point(self):
+        calculate('2.7.2')
