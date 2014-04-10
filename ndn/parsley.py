be08dad86249065533820835c4daf13961d36ec7
Andrew Lorente <hello@andrewlorente.com>
2014-04-09 19:47:31 -0700

    Now our parser can add two numbers [parsley]
    Notice that the `+` rule returns a function. This seems a little
    overcomplicated now, but when we start adding operators you'll see that
    it provides a certain elegance.
    
    Notice also that we had to change the function called by calculate().
    The Parsley function makeGrammar returns a function that takes a piece
    of text and returns a parser; that parser has functions with names
    corresponding to the rules defined in the grammar. Since our grammar's
    outermost rule is now expression, that's the function we should call.

--- ndn/parsley.py
+++ ndn/parsley.py
@@ -3,8 +3,11 @@ from __future__ import absolute_import
 from parsley import makeGrammar
 
 grammar = makeGrammar("""
-    number = <'-'? digit+ ('.' digit+)?>:ds -> float(ds)
+    expression = addition | number
+    addition   = number:a plus:op number:b -> op(a, b)
+    number     = <'-'? digit+ ('.' digit+)?>:ds -> float(ds)
+    plus       = '+' -> lambda a, b: a + b
 """, {})
 
 def calculate(text):
-    return grammar(text).number()
+    return grammar(text).expression()
